eec718c6840269202e9b1b684d6abad3c2dc78f2
Anirudh-N <Anirudh.Narasimhamurthy@utah.edu>
2016-03-16 14:06:20 -0600

    Adding the sql file for creating inventory table in database

---
+++ running_inventory.sql
@@ -0,0 +1,25 @@
+create database izi;
+
+use izi;
+
+/* If create table fails, drop table and then run create table 
+Command: drop table running_inventory */
+
+create table running_inventory
+(
+food_id int,
+food_name varchar(30),
+food_qty int,
+waste int default 0
+);
+
+alter table running_inventory
+add constraint pk_inventory primary key(food_id);
+
+insert into running_inventory(food_id,food_name,food_qty) values (1,'horse_food',50);
+insert into running_inventory(food_id,food_name,food_qty) values (2,'zebra_food',50);
+insert into running_inventory(food_id,food_name,food_qty) values (3,'lion_food',50);
+insert into running_inventory(food_id,food_name,food_qty) values (4,'tiger_food',50);
+
+
+select * from running_inventory
